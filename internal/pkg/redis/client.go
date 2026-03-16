a24069fed035857b111e9405f1057b495fdfc6b4
Abdulrahman <abdulrahman939291@gmail.com>
2026-03-16 10:29:45 +0100

    feat(redis): setup redis client for session management and caching [ECS-57]

---
+++ internal/pkg/redis/client.go
@@ -0,0 +1,12 @@
+package redis
+
+import (
+    "context"
+    "github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient(addr string) *redis.Client {
+    return redis.NewClient(&redis.Options{
+        Addr: addr,
+    })
+}
